7ddebea31baf302b50beb0a725df77cfd304094d
Chirag Masekar <cmasekar@gmail.com>
2018-08-10 14:24:23 -0400

    working end-to-end. need refactor around verify email wait

--- main.py
+++ main.py
@@ -5,17 +5,13 @@ import selenium_helpers
 outlook_email = "virtru-challenge@outlook.com"
 outlook_password = "!nsecur3Pa$$word"
 
-# options = webdriver.ChromeOptions()
-# options.add_argument('--ignore-certificate-errors')
-# options.add_argument("--test-type")
-# options.binary_location = "/usr/bin/chromium"
 driver = webdriver.Firefox()
 driver.maximize_window()
 driver.get('https://outlook.live.com/owa/')
 
 encrypted_email_subject = ''
 chosen_email = ''
-email_content = ''
+email_content = 'Sed dignissim netus arcu id egestas ultricies lacus etiam libero mattis augue sit risus, neque magna 🍺🍒💍🏰🎻🐄🐩🕘🍥📊🏯📋💟🎦 mauris facilisis vestibulum eleifend 🔁🐅🎮🐘 nec 🕧🔯👪👸🍕🏆 sit lacus pulvinar quisque morbi maecenas 🌏👠🌐🎲 🍃👡📙💫👅💣 🔚🔞🐊🌗🐾📍🏤🎈💙 mattis suspendisse eu euismod 🍨🎻🍓🕘🏡📤👈🎊🍤📦. Sit ante maecenas pellentesque nibh dignissim amet eget sit sed nulla 🍢🔋🎾🎻🍆🌁🐹🎵🐗🔭 feugiat orci ipsum viverra sit orci condimentum ultricies leo diam orci et orci, nec quam blandit. 🍁🔴🏰🎤💵🌖📔🍤🍈👅👾📕👉🌑 🎿🌶🍒🍸🏧🔤🍸🌒👌🔎💓🍩📌💗 vestibulum vitae dui pellentesque lobortis et sit id pellentesque diam at 🌼👒🗽🎉🔞💒💃👋🌳📡🍋🕜🐭🌻 ipsum convallis dapibus urna faucibus integer cras 🐏🔨🐲🌗🕣👐💠 aenean sed molestie semper a aliquet magna interdum et luctus quis commodo, felis est porttitor commodo.'
 
 
 outlook_sign_in_xpath = "//*[@class='linkButtonFixedHeader office-signIn']"
@@ -23,11 +19,9 @@ email_input_xpath = "//input[@type='email']"
 email_submit_button_xpath = "//input[@type='submit'][@value='Next']"
 password_submit_button_xpath = "//input[@type='submit'][@value='Sign in']"
 email_password_inputxpath = "//input[@type='password']"
-
-
 virtru_link_regex = '^(https?:\/\/)?(secure)\.(virtru)\.(com)\//?'
-ms_doughboy_xpath = "//*[@class='o365cs-mfp-header']//div[@class='o365cs-mfp-doughboy-container']"
 
+ms_doughboy_xpath = "//*[@class='o365cs-mfp-header']//div[@class='o365cs-mfp-doughboy-container']"
 unlock_message_link_xpath = "//a[starts-with(@href, 'https://secure.virtru.com/start/?')][text()[contains(.,'Unlock Message')]]"
 verify_me_link_xpath = "//a[starts-with(@href, 'https://accounts.virtru.com/email-activation?linkId=')][text()[contains(.,'VERIFY ME')]]"
 
@@ -81,6 +75,9 @@ def click_virtru_unlock_message_link(email):
 
 
 def verify_virtru(email):
+    driver.refresh()
+    selenium_helpers.get_clickable_element_by_xpath(
+        driver, ms_doughboy_xpath, 15)
     selenium_helpers.wait_and_click_element_by_xpath(
         driver, "//*[@role='menu']//span[@title='Other']", 5)
     driver.implicitly_wait(3)
@@ -97,10 +94,18 @@ def assert_email_contents(expected_email_contents):
 
     selenium_helpers.get_clickable_element_by_xpath(
         driver, "//div[@id='click-into-reply-view']", 5)
-    raise NotImplementedError
+
+    # get text from email body
+    encryped_email_body = selenium_helpers.get_clickable_element_by_xpath(driver,"//*[@id='tdf-body']",5)
+    actual_email_contents = encryped_email_body.text
+    print(actual_email_contents)
+    # assert email text equal to expected_email_contents
+
+    assert expected_email_contents == actual_email_contents
 
 
 sign_in(outlook_email, outlook_password)
-open_email_with_subject("manual-test")
+open_email_with_subject("automated-emoji-test")
 click_virtru_unlock_message_link(outlook_email)
 verify_virtru(outlook_email)
+assert_email_contents(email_content)
